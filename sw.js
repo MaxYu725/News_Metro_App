86364ed8c71092fe4a2e36c3790c585e7eafb7b9
MaxYu725 <maxyu0725@outlook.com>
2026-08-16 23:31:58 +0800

    MUI-4C: cache final polish module

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'metro-news-cache-v11-mui4b';
+const CACHE_NAME = 'metro-news-cache-v12-mui4c';
 
 const URLS_TO_CACHE = [
     './',
@@ -10,6 +10,7 @@ const URLS_TO_CACHE = [
     './feed-ui.js',
     './search-ui.js',
     './reader-ui.js',
+    './mui4c-polish.js',
     './api.js',
     './utils.js',
     './tracking.js',
