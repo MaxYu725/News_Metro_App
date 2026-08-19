7287b7edb7b41981a0c39cfcf77c5e0e467625b2
MaxYu725 <maxyu0725@outlook.com>
2026-08-19 19:09:24 +0800

    MUI-7C.2 refresh PWA shell for cutout color

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const APP_CACHE_PREFIX = 'metro-news-';
-const SHELL_CACHE = 'metro-news-shell-v38-shell-spacing-refinement';
+const SHELL_CACHE = 'metro-news-shell-v39-android-cutout-blend';
 const RUNTIME_CACHE = 'metro-news-runtime-v1';
 
 const SHELL_URLS = [
