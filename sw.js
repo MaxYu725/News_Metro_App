72250424ca0c7875f5fc61257a3897ec6bfe617e
MaxYu725 <maxyu0725@outlook.com>
2026-08-30 23:32:33 +0800

    Bump PWA cache for Reader compositor fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const APP_CACHE_PREFIX = 'metro-news-';
-const SHELL_CACHE = 'metro-news-shell-v41-reader-return-smoothness';
+const SHELL_CACHE = 'metro-news-shell-v42-reader-compositor-fix';
 const RUNTIME_CACHE = 'metro-news-runtime-v1';
 
 const SHELL_URLS = [
