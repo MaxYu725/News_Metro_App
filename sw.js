913079882040060a534f6939cb931881c09d22e4
MaxYu725 <maxyu0725@outlook.com>
2026-09-03 20:55:26 +0800

    Bump PWA shell for snap-release press feedback

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const APP_CACHE_PREFIX = 'metro-news-';
-const SHELL_CACHE = 'metro-news-shell-v50-visible-press-feedback';
+const SHELL_CACHE = 'metro-news-shell-v51-press-snap-release';
 const RUNTIME_CACHE = 'metro-news-runtime-v1';
 
 const SHELL_URLS = [
