623f4030da88ef74d007dc5ad47bbec868641459
MaxYu725 <maxyu0725@outlook.com>
2026-09-16 10:18:33 +0800

    deploy: bump v73 Reader performance shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const APP_CACHE_PREFIX = 'metro-news-';
-const SHELL_CACHE = 'metro-news-shell-v72-feed-cursor-pagination';
+const SHELL_CACHE = 'metro-news-shell-v73-reader-performance';
 const RUNTIME_CACHE = 'metro-news-runtime-v1';
 
 const SHELL_URLS = [
